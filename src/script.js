4233c5edb4f92e2148fac5f8d8866cc7b2449de7
mikhail nikonov <>
2018-12-19 22:03:59 +0100

    semi-black version with comments

--- src/script.js
+++ src/script.js
@@ -1,42 +1,67 @@
-var text = "aems",
-    height = 3,
+let rating;
+
+//get this value from html
+//<------NUMBER----
+let numberFromBlock = '9.5';
+//<------NUMBER-------->
+
+//Scene
+let scene = new THREE.Scene();
+
+//Camera
+let camera = new THREE.PerspectiveCamera( 60, window.innerWidth/window.innerHeight, .1, 1000);
+camera.position.set(0,10,30);
+camera.up = new THREE.Vector3(0,0,-2);
+camera.lookAt(new THREE.Vector3(0,0,-2));
+
+//Render
+let renderer = new THREE.WebGLRenderer({alpha: true});
+//render transparent scene
+renderer.setClearColor( 0x000000, 0 );
+//apply scene to html
+document.getElementById("canvas").appendChild( renderer.domElement );
+
+//Used material
+let material = new THREE.MeshPhongMaterial( { color: 0xff1488  } );
+
+
+//Light
+let light = new THREE.DirectionalLight( 0xffffff, 1 );
+light.position.set( -5, 5, 10 );
+
+
+
+//Loading font and rendering scene
+let height = 3,
     size = 13,
     curveSegments = 12,
     font = undefined;
-var cube;
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 50, window.innerWidth/window.innerHeight,.1, 1000);
-
-var renderer = new THREE.WebGLRenderer();
-document.getElementById("canvas").appendChild( renderer.domElement );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
-
-camera.position.z = 30;
-
-var animate = function () {
-    requestAnimationFrame( animate );
-    cube.rotation.y += 0.05;
-
-    renderer.render( scene, camera );
-};
-
-var loader = new THREE.FontLoader();
-
+let loader = new THREE.FontLoader();
 loader.load( './src/font.js', function ( font ) {
-    console.log(font);
-    var geo_font = new THREE.TextGeometry( '9.5', {
+    let geo_font = new THREE.TextGeometry( numberFromBlock, {
         font: font,
         size: size,
         height: height,
         curveSegments:curveSegments,
         weight: "normal"
+
     } );
     geo_font.center();
-    cube = new THREE.Mesh( geo_font, material );
+    rating = new THREE.Mesh( geo_font, material );
+    rating.scale.x = 0.7;
     scene.lookAt(camera.position);
-    scene.add( cube );
-    console.log(cube);
+    scene.add( camera );
+    scene.add( light );
+    scene.add( rating );
 
     animate();
 } );
+
+//Rotat animation
+const animate = function () {
+    requestAnimationFrame( animate );
+    rating.rotation.y += 0.03;
+
+    renderer.render( scene, camera );
+};
