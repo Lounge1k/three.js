ff893e0c5c9aaff018ad44dff13c9a585ca1435c
mikhail nikonov <>
2018-12-21 11:15:40 +0100

    final version x2

--- src/script.js
+++ src/script.js
@@ -1,16 +1,16 @@
 let rating;
 
 //get this value from html
-//<------NUMBER----
-let numberFromBlock = '3.5';
+//<------NUMBER-------->
+let numberFromBlock = '8.5';
 //<------NUMBER-------->
 
 //Scene
 let scene = new THREE.Scene();
 
 //Camera
-let camera = new THREE.PerspectiveCamera( 100, window.innerWidth/window.innerHeight, .1, 1000);
-camera.position.set(0,0,15);
+let camera = new THREE.PerspectiveCamera( 50, window.innerWidth/window.innerHeight, .1, 1000);
+camera.position.set(0,0,30);
 camera.up = new THREE.Vector3(0,0,-2);
 camera.lookAt(new THREE.Vector3(0,0,-2));
 
@@ -22,7 +22,10 @@ renderer.setClearColor( 0x000000, 0 );
 
 //apply renderer to html
 //PLACE NAME OF A BLOCK HERE
+//<-------------------------->
 document.querySelector("#canvas").appendChild( renderer.domElement );
+//<-------------------------->
+
 
 //Used material
 let material = new THREE.MeshPhongMaterial( { color: 0xff1488  } );
@@ -62,6 +65,5 @@ loader.load( './src/font.json', function ( font ) {
 const animate = function () {
     requestAnimationFrame( animate );
     rating.rotation.y += 0.03;
-
     renderer.render( scene, camera );
 };
